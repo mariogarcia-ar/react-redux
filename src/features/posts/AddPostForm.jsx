aec27c56e0571461e146e8afc9a3f162f4e6223a
Mario Garcia <mariogarcia.ar@gmail.com>
2024-05-03 22:24:40 -0300

    refactoring using reduce/prepare

--- src/features/posts/AddPostForm.jsx
+++ src/features/posts/AddPostForm.jsx
@@ -1,6 +1,5 @@
 import React, { useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { nanoid } from '@reduxjs/toolkit'
 
 import { postAdded } from './postsSlice'
 
@@ -17,9 +16,9 @@ function AddPostForm() {
 
     const onSavePostClicked = () => {
         if (title && content) {
-            dispatch(postAdded({ id: nanoid(), title, content }))
+            dispatch(postAdded(title, content))
             setTitle('')
-            // setContent('')
+            setContent('')
         }
     }
 
@@ -34,8 +33,9 @@ function AddPostForm() {
             
             <label htmlFor="postContent">post content:</label>
             <textarea name="postContent" id="postContent" cols="30" rows="10"
+                    value={content}
                     onChange={onContentChange}
-              >{content}</textarea>
+              ></textarea>
 
             <button type="button"
               onClick={onSavePostClicked}
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from '@reduxjs/toolkit'
+
 
 const initialState = [
     {id: 1, title: 'First Post!', content: 'Hello!'},
@@ -10,8 +12,19 @@ export const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
-        postAdded(state, action) {
-            state.push(action.payload)
+        postAdded: {
+            reducer(state, action) {
+                state.push(action.payload)
+            },
+            prepare(title, content) {
+                return {
+                    payload: {
+                        id: nanoid(),
+                        title,
+                        content
+                    }
+                }
+            },         
         },
     },
 });
