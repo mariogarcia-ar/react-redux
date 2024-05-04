11fcceea6992f73746bb8578a3c66aecd24fbe6f
Mario Garcia <mariogarcia.ar@gmail.com>
2024-05-03 21:18:22 -0300

    Add reset and incrementByAmount

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { increment, decrement } from './counterSlice'
+import { increment, decrement, reset, incrementByAmount } from './counterSlice'
 import './Counter.css'
 
 
@@ -8,7 +8,8 @@ function Counter() {
     const count = useSelector(state => state.counter.value)
     const dispatch = useDispatch()
     const [innerCount, setInnerCount] = useState(0);
-
+    const [incrementAmount, setIncrementAmount] = useState(0);
+    const addValue = Number(incrementAmount) || 0;
 
     const  _onDecrement = () => {
         setInnerCount(innerCount - 1)
@@ -24,6 +25,12 @@ function Counter() {
         Counter: {count}
         <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(decrement())}>-</button>
+        <button onClick={() => dispatch(reset())}>R</button>
+        <div>
+          incrementAmount: {incrementAmount}
+          <input type="text" value={incrementAmount} onChange={(e) => setIncrementAmount(e.target.value)} />
+          <button onClick={() => dispatch(incrementByAmount(addValue))}>+ by value</button>
+        </div>
     </section>
         <section>
             Inner Counter: {innerCount}
